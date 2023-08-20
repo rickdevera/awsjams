0a954a2bca25018a922e52dcfa051399d248c55d
rickdevera <44529212+rickdevera@users.noreply.github.com>
2023-08-20 15:27:41 -0500

    adding information

--- tcs_setup.sh
+++ tcs_setup.sh
@@ -2,9 +2,11 @@
 # Script file to be used to for container security
 
 #Copy latest code from tenable
+echo "Downloading..."
 curl https://www.tenable.com/downloads/api/v2/pages/tenable-cs/files/tenable.cs-cli_latest_Linux_x86_64.tar.gz --output tenable.cs-cli_latest_Linux_x86_64.tar.gz
 
 # extract it
+echo "Extracting..."
 tar xzf tenable.cs-cli_latest_Linux_x86_64.tar.gz
 
 # make it work
